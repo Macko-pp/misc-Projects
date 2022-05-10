976b2aba295347f5c3dcd7273c90a1e53286f481
Macko++ <max.konietzko.maya@gmail.com>
2022-05-10 15:01:00 -0500

    Added some more lines and reduced the initial time

--- keyPress.py
+++ keyPress.py
@@ -1,21 +1,22 @@
 import os
 import pyautogui
-# from pynput.keyboard import Key, Controller
-
-# keyboard = Controller()
 
 text = [
-    "Hello my child...",
+    "Hello, my child...",
+    "I'm the FBI agent youre always talking about...",
     "I've been watching you...",
-    "You have been very naughty my boy...",
-    "We do not tolerate this boy...",
-    "We are on our way now boy...",
-    "Anyways see you later BYE!"
+    "You have been very naughty, my boy...",
+    "We do not tolerate this, boy...",
+    "We are on our way now, boy...",
+    "Be carefull, boy...",
+    "Remember...",
+    "We are ALWAYS watching...",
+    "〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠〠"
 ]
 
-os.system("sleep 4")
+os.system("sleep 1")
 
-for i in range(0, 6):
+for i in range(len(text)):
     os.system("sleep 4")
     pyautogui.press("enter")
     pyautogui.typewrite(text[i])
