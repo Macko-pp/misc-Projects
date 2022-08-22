e4e8ad29aa49ed07fc66a9cd886481bd928df60b
Macko++ <max.konietzko.maya@gmail.com>
2022-08-22 16:12:00 -0500

    Started making some new shapes

--- mouseLineDrawer.py
+++ mouseLineDrawer.py
@@ -4,35 +4,17 @@ import random
 
 os.system("sleep 2")
 
-# ranX = random.randrange(62, 1439)
-# ranY = random.randrange(161, 826)
+# for i in range(1, 50):
+# 	ranX1 = random.randrange(62, 1439)
+# 	ranY1 = random.randrange(161, 826)
 
-# pyautogui.mouseDown(ranX, ranY)
-
-for i in range(1, 50):
-	ranX1 = random.randrange(62, 1439)
-	ranY1 = random.randrange(161, 826)
-
-	pyautogui.mouseDown()
-	pyautogui.moveTo(ranX1, ranY1, 0)
-	pyautogui.mouseUp()
+# 	pyautogui.mouseDown()
+# 	pyautogui.moveTo(ranX1, ranY1, 0)
+# 	pyautogui.mouseUp()
 
 def drawLine(x, y, x1, y1):
 	pyautogui.mouseDown(x, y)
-	pyautogui.moveTo(x1, y1, 0.25)
+	pyautogui.moveTo(x1, y1, 0)
 	pyautogui.mouseUp(x1, y1)
 
-# drawLine(550, 620, 860, 620)
-# drawLine(860, 620, 860, 340)
-# drawLine(860, 340, 550, 340)
-# drawLine(550, 340, 550, 620)
 
-# drawLine(550 + 100, 620 + 100, 860 + 100, 620 + 100)
-# drawLine(860 + 100, 620 + 100, 860 + 100, 340 + 100)
-# drawLine(860 + 100, 340 + 100, 550 + 100, 340 + 100)
-# drawLine(550 + 100, 340 + 100, 550 + 100, 620 + 100)
-
-# drawLine(550 + 100, 620 + 100, 550, 620)
-# drawLine(860 + 100, 620 + 100, 860, 620)
-# drawLine(860 + 100, 340 + 100, 860, 340)
-# drawLine(550 + 100, 340 + 100, 550, 340)
