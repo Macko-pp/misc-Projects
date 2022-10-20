9968d8a0b1b94d2d2e2522f79250dff89a97413f
Macko++ <max.konietzko.maya@gmail.com>
2022-10-20 11:47:59 -0500

    Added a progress bar and made it switch tabs

--- ixlCheat.py
+++ ixlCheat.py
@@ -1,15 +1,26 @@
 from time import sleep
-import os
-import pyautogui as gui
+from pyautogui import click
 from tqdm import tqdm
+from playsound import playsound
 
-for i in range(0, 5):
-
+for i in range(0, 15):
     for i in tqdm(range(0, 300)):
-
         if i == 290:
-            os.system("afplay '/Applications/Desktop\ Goose.app/Contents/Resources/Honk1.mp3")
+            playsound("/Users/maxkonietzko/Python/Bell.mp3")
 
         sleep(1)
 
-    gui.click(85, 388)
+    click(19, 23)
+    sleep(0.5)
+
+    for i in range(1, 9):
+        click(((i + 2) * 100), 450)
+        sleep(0.05)
+
+    for i in range(1, 9):
+        click(((i + 2) * 100), 464)
+        sleep(0.05)
+
+    click(1000, 518)
+    sleep(1)
+    click(155, 23)
